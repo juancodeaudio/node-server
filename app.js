f5e853400804049c11d2862ae2099de3859823cd
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2024-07-13 20:16:09 -0500

    Transform project to ESModules

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require('express') // require -> commonJS
-const crypto = require('node:crypto')
-const cors = require('cors')
+import express, { json } from 'express' // require -> commonJS
+import { randomUUID } from 'node:crypto'
+import cors from 'cors'
 
-const movies = require('./movies.json')
-const { validateMovie, validatePartialMovie } = require('./schemas/movies')
+import movies from './movies.json' with { type: 'json'}
+import { validateMovie, validatePartialMovie } from './schemas/movies.js'
 
 const app = express()
-app.use(express.json())
+app.use(json())
 app.use(cors({
   origin: (origin, callback) => {
     const ACCEPTED_ORIGINS = [
@@ -64,7 +64,7 @@ app.post('/movies', (req, res) => {
 
   // en base de datos
   const newMovie = {
-    id: crypto.randomUUID(), // uuid v4
+    id: randomUUID(), // uuid v4
     ...result.data
   }
 
