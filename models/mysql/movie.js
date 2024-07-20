aaa8391757d26f10db4572c1c03ad1da068e0aaa
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2024-07-20 17:41:09 -0500

    Update movie genre filtering in MovieModel

--- models/mysql/movie.js
+++ models/mysql/movie.js
@@ -4,14 +4,42 @@ const config = {
   host: 'localhost',
   user: 'root',
   port: 3306,
-  passsword: '123',
+  password: '123',
   database: 'moviesdb'
 }
 
 const connection = await mysql.createConnection(config)
 
 export class MovieModel {
-  static async getAll ({ genre }) {}
+  static async getAll ({ genre }) {
+    if (genre) {
+      const lowerCaseGenre = genre.toLowerCase()
+      // Get genre ids from database table using genre names
+      const [genres] = await connection.query(
+        `SELECT id, name
+        FROM genre
+        WHERE LOWER(name) = ?;`,
+        [lowerCaseGenre]
+      )
+
+      // If genre not found, return empty array
+      if (genres.length === 0) {
+        return []
+      }
+
+      // Get id from first genre found
+      // const [{ id }] = genres
+
+      // Get movie data from movie table using movieIDs using JOIN
+      return []
+    }
+    const [movies] = await connection.query(
+      `SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) id
+      FROM movie;`
+    )
+
+    return movies
+  }
 
   static async getById ({ id }) {}
 
