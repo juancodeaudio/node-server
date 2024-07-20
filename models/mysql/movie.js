ccedf0b45a21631e2ead8f46770eca8f7fd12c59
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2024-07-20 17:41:32 -0500

    Add getById method to MovieModel

--- models/mysql/movie.js
+++ models/mysql/movie.js
@@ -41,7 +41,20 @@ export class MovieModel {
     return movies
   }
 
-  static async getById ({ id }) {}
+  static async getById ({ id }) {
+    const [movies] = await connection.query(
+      `SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) id
+      FROM movie
+      WHERE id = UUID_TO_BIN(?);`,
+      [id]
+    )
+
+    if (movies.length === 0) {
+      return null
+    }
+
+    return movies[0]
+  }
 
   static async create ({ input }) {}
 
